5f2cb8b42ac18ac04953ba083d270e370ba900a6
Sam Okello <samokello024@gmail.com>
2025-11-26 17:02:06 +0300

    modified sever index.js

--- server/index.js
+++ server/index.js
@@ -87,7 +87,7 @@ app.use('/api/auth/', authLimiter);
 // Basic middleware
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'www.rebirthofaqueen.org'); // Replace with your actual frontend domain
+  res.header('Access-Control-Allow-Origin', '*'); // Replace with your actual frontend domain
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
